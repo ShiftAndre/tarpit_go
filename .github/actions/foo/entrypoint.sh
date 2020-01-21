c0e228474671ac2c742fd54d9e10a626d479d8d5
Preetam Jinka <preetam@shiftleft.io>
2020-01-21 12:12:38 -0800

    fix flag

--- .github/actions/foo/entrypoint.sh
+++ .github/actions/foo/entrypoint.sh
@@ -2,12 +2,12 @@
 
 echo "Started"
 curl -XPOST "https://api.github.com/repos/$GITHUB_REPO/statuses/$GITHUB_SHA" \
-  -h "Authorization: Bearer $GITHUB_TOKEN" \
-  -h "Content-Type: application/json" \
+  -H "Authorization: Bearer $GITHUB_TOKEN" \
+  -H "Content-Type: application/json" \
   -d '{"state": "pending", "context": "foo"}'
 sleep 10
 curl -XPOST "https://api.github.com/repos/$GITHUB_REPO/statuses/$GITHUB_SHA" \
-  -h "Authorization: Bearer $GITHUB_TOKEN" \
-  -h "Content-Type: application/json" \
+  -H "Authorization: Bearer $GITHUB_TOKEN" \
+  -H "Content-Type: application/json" \
   -d '{"state": "success", "context": "foo"}'
 echo "Ended"
