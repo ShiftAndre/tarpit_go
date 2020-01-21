7a471064bf8ece054e1177b82cfb832295e2119d
Preetam Jinka <preetam@shiftleft.io>
2020-01-21 12:10:37 -0800

    add status

--- .github/actions/foo/entrypoint.sh
+++ .github/actions/foo/entrypoint.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
 echo "Started"
+curl -XPOST "https://api.github.com/repos/$GITHUB_REPO/statuses/$GITHUB_SHA" \
+  -h "Authorization: Bearer $GITHUB_TOKEN" \
+  -h "Content-Type: application/json" \
+  -d '{"state": "pending", "context": "foo"}'
 sleep 10
+curl -XPOST "https://api.github.com/repos/$GITHUB_REPO/statuses/$GITHUB_SHA" \
+  -h "Authorization: Bearer $GITHUB_TOKEN" \
+  -h "Content-Type: application/json" \
+  -d '{"state": "success", "context": "foo"}'
 echo "Ended"
