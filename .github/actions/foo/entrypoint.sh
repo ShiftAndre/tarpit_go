fb4aa46cfcb80410eb9fbf88f65a09b110249a5d
Preetam Jinka <preetam@shiftleft.io>
2020-01-21 12:15:33 -0800

    tweak statuses

--- .github/actions/foo/entrypoint.sh
+++ .github/actions/foo/entrypoint.sh
@@ -4,10 +4,10 @@ echo "Started"
 curl -XPOST "https://api.github.com/repos/$GITHUB_REPO/statuses/$GITHUB_SHA" \
   -H "Authorization: Bearer $GITHUB_TOKEN" \
   -H "Content-Type: application/json" \
-  -d '{"state": "pending", "context": "foo"}'
+  -d '{"state": "pending", "context": "Security checks", "target_url": "https://example.com"}'
 sleep 10
 curl -XPOST "https://api.github.com/repos/$GITHUB_REPO/statuses/$GITHUB_SHA" \
   -H "Authorization: Bearer $GITHUB_TOKEN" \
   -H "Content-Type: application/json" \
-  -d '{"state": "success", "context": "foo"}'
+  -d '{"state": "failure", "context": "Security checks", "target_url": "https://example.com"}'
 echo "Ended"
